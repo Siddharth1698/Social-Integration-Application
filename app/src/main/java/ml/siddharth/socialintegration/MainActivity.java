ecfe2b92b1f80b0483cea8b63eb2a5c05bcf46ec
Siddharth M <siddharth.m98@gmail.com>
2019-05-12 12:40:39 +0530

    Integration and detail revival

--- app/src/main/java/ml/siddharth/socialintegration/MainActivity.java
+++ app/src/main/java/ml/siddharth/socialintegration/MainActivity.java
@@ -1,15 +1,115 @@
 package ml.siddharth.socialintegration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView timg;
+    private CallbackManager callbackManager;
+    private TextView tname,temail;
+    private LoginButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timg = (ImageView)findViewById(R.id.profileimage);
+        tname = (TextView)findViewById(R.id.name);
+        temail = (TextView)findViewById(R.id.email);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions(Arrays.asList("email","public_profile"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+
+
+        });
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+       AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+           @Override
+           protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+               if (currentAccessToken == null){
+                   tname.setText("");
+                   temail.setText("");
+                   timg.setImageResource(0);
+                   Toast.makeText(getApplicationContext(),"User logged out",Toast.LENGTH_SHORT).show();
+               }else {
+                   loaduserProfile(currentAccessToken);
+               }
+
+           }
+       };
+    private void loaduserProfile(AccessToken newAccessToken){
+        GraphRequest graphRequest = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+
+                    String image_url = "https://graph.facebook.com/"+id+"/picture?type=normal";
+
+                    temail.setText(email);
+                    tname.setText(first_name + " " + last_name);
+                    Picasso.get().load(image_url).into(timg);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("feilds","first_name,last_name,email,id");
+        graphRequest.setParameters(parameters);
+        graphRequest.executeAsync();
     }
 }
